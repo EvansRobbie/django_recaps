c723e204bbe674f653ccd8cb40ea428a0b3930ff
EvansRobbie <evansitoden94@gmail.com>
2023-07-16 00:28:35 +0300

    removing hardcorded URLs

--- music/views.py
+++ music/views.py
@@ -1,4 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+# from django.template import loader
+from django.shortcuts import render
+from .models import Album
 
 def index(request):
-    return HttpResponse("<h1>This is a Music app page</h1>")
+    all_albums = Album.objects.all()
+    # template = loader.get_template('music/index.html')
+    context = {"all_albums": all_albums }
+    return render(request, 'music/index.html', context)
+
+def detail(request, pk):
+    try:
+        album = Album.objects.get(pk=pk)
+        context = {"album": album}
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', context)
