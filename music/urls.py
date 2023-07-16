d8213028dee363097a416ee3ab689ecc9e64de1e
EvansRobbie <evansitoden94@gmail.com>
2023-07-16 22:03:05 +0300

    generic views

--- music/urls.py
+++ music/urls.py
@@ -4,7 +4,11 @@ from . import views
 app_name = "music"
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('<int:pk>/', views.detail, name="detail"),
-    path('<int:pk>/favorite/', views.favorite, name="favorite")
+    path('', views.IndexView.as_view(), name="index"),
+    path('<int:pk>/', views.DetailView.as_view(), name="detail"),
+    path('album/add/', views.AlbumCreate.as_view(), name = 'album-add'),
+    path('album/<int:pk>/', views.AlbumUpdate.as_view(), name = 'album-update'),
+    path('album/<int:pk>/delete/', views.AlbumDelete.as_view(), name = 'album-delete'),
+    path('register/', views.UserFormView.as_view(), name="register"),
 ]
+
--- music/views.py
+++ music/views.py
@@ -1,31 +1,62 @@
 # from django.http import HttpResponse, Http404
 # from django.template import loader
-from django.shortcuts import render, get_object_or_404
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from .models import Album, Song
+from .forms import UserForm
 
-def index(request):
-    all_albums = Album.objects.all()
-    # template = loader.get_template('music/index.html')
-    context = {"all_albums": all_albums }
-    return render(request, 'music/index.html', context)
+class IndexView(ListView):
+    template_name = "music/index.html"
+    context_object_name = "all_albums"
 
-def detail(request, pk):
-    album = get_object_or_404(Album, pk=pk)
-    context = {"album": album}
-    # try:
-    #     album = Album.objects.get(pk=pk)
-    #     
-    # except Album.DoesNotExist:
-    #     raise Http404("Album does not exist")
-    return render(request, 'music/detail.html', context)
+    def get_queryset(self):
+        return Album.objects.all()
+    
+class DetailView(DetailView):
+    model = Album
+    template_name = 'music/detail.html'
 
-def favorite(request, pk):
-    album = get_object_or_404(Album, pk=pk)
-    try:
-        selected_song = album.song_set.get(pk=request.POST["song"])
-    except (KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html', {"album": album, "error_message": "you did not select a valid song"})
-    else:
-        selected_song.is_favorite = True
-        selected_song.save()
-        return render(request, 'music/detail.html', {"album": album})
+class AlbumCreate(CreateView):
+    model = Album
+    fields = "__all__"
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = "__all__"
+
+class AlbumDelete(DeleteView):
+    model = Album
+    fields = "__all__"
+    success_url = reverse_lazy('music:index')
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'music/registration_form.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form':form})
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            # cleaned (normalised) data // unifies data
+            username = form.cleaned_data(['username'])
+            password = form.cleaned_data(['password'])
+            user.set_password(password)
+            user.save()
+
+            # returns user objects if credentials are correct
+            user = authenticate(username=username, password=password)
+            
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('music:index')
+            
+        return render(request, self.template_name, {'form':form})
